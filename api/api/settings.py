70f02799c3e99a64be49542e5e3a973bcb228bcc
VirtualSpaceman <levy.gu@gmail.com>
2018-03-30 00:04:57 -0300

    heroku

--- api/api/settings.py
+++ api/api/settings.py
@@ -98,9 +98,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR'
+LANGUAGE_CODE = 'en-US'
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
